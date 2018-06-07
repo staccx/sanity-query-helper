c36bf3b3d5e32ab0316394989ab55681e2550489
Espen Th Sæverud <espensaeverud@gmail.com>
2018-06-07 13:08:03 +0200

    docs: Added documentation to functions.

--- src/sanityQueryHelper.js
+++ src/sanityQueryHelper.js
@@ -5,7 +5,15 @@ const generateProjections = projections =>
   projections ? `{${projections}}` : "{...}"
 const generateSelector = selector => selector
 
+/**
+ * @class
+ */
 class SanityQueryHelper {
+  /**
+   * SanityQueryHelper constructor. Mininum new SanityQueryHelper({sanityOptions})
+   * @constructor
+   * @param {object} sanityOptions - The same options you would pass to the sanityClient
+   */
   constructor (
     {
       sanityOptions,
@@ -30,40 +38,88 @@ class SanityQueryHelper {
     }
   }
 
+  /**
+   * Creates a test of type e.g _type == "myType"
+   * @param {string} type - The name of the type ex. "post"
+   * @returns {SanityQueryHelper}
+   */
   ofType (type) {
     return new SanityQueryHelper(this)
       .withFilter("_type")
       .equalTo(`"${type}"`)
   }
 
+  /**
+   * Add filter. NOTE: You will need to call compare functions after this e.g withfilter("realeaseYear").greaterThan(2000)
+   * @param {string} filter
+   * @returns {SanityQueryHelper}
+   */
   withFilter (filter) {
     return new SanityQueryHelper(this, filter)
   }
 
+  /**
+   * Creates a filter with (filter == value) e.g releaseYear == 1999
+   * @param {string} value - Value to compare against
+   * @returns {SanityQueryHelper}
+   */
   equalTo (value) {
     return this.compare(value, "==", this.filter)
   }
 
+  /**
+   * Creates a filter with (filter != value) e.g movieType != "drama"
+   * @param {string} value - Value to compare against
+   * @returns {SanityQueryHelper}
+   */
   notEqualTo (value) {
     return this.compare(value, "!=", this.filter)
   }
 
+  /**
+   * Creates a filter with (filter < value) e.g members < 5
+   * @param {string} value - Value to compare against
+   * @returns {SanityQueryHelper}
+   */
   lessThan (value) {
     return this.compare(value, "<", this.filter)
   }
 
+  /**
+   * Creates a filter with (filter <= value) e.g members <= 5
+   * @param {string} value - Value to compare against
+   * @returns {SanityQueryHelper}
+   */
   lessOrEqualTo (value) {
     return this.compare(value, "<=", this.filter)
   }
 
+  /**
+   * Creates a filter with (filter > value) e.g members > 5
+   * @param {string} value - Value to compare against
+   * @returns {SanityQueryHelper}
+   */
   greaterThan (value) {
     return this.compare(value, ">", this.filter)
   }
 
+  /**
+   * Creates a filter with (filter <= value) e.g members <= 5
+   * @param {string} value - Value to compare against
+   * @returns {SanityQueryHelper}
+   */
   greaterOrEqualTo (value) {
     return this.compare(value, ">=", this.filter)
   }
 
+  /**
+   * Used to create the filters.
+   * @example compare("releaseYear", "==", 2000)
+   * @param value
+   * @param operation
+   * @param key
+   * @returns {SanityQueryHelper}
+   */
   compare (value, operation, key) {
     if (!key) {
       console.warn(
@@ -75,21 +131,44 @@ class SanityQueryHelper {
     return new SanityQueryHelper(this, null)
   }
 
+  /**
+   * Adds an ordering variable to the GROQ query e.g order(releaseYear)
+   * @example helper.orderBy("releaseYear") -> | order(releaseYear)
+   * @param order
+   * @returns {SanityQueryHelper}
+   */
   orderBy (order) {
     this.order = order
     return new SanityQueryHelper(this, null)
   }
 
+  /**
+   * Picks fields from result. In GROQ-speak this is called projections
+   * @link https://www.sanity.io/docs/data-store/how-queries-work#projections
+   * @param projections
+   * @returns {SanityQueryHelper}
+   */
   project (projections) {
     this.projections = projections
     return new SanityQueryHelper(this, null)
   }
 
+  /**
+   * Adds select range. By default sanity selects [0..100]
+   * @param {number} from - Index to start from
+   * @param {number} to - Index to end
+   * @param inclusive - Whether or not to include {to} index value
+   * @returns {SanityQueryHelper}
+   */
   select (from, to, inclusive = false) {
     this.selector = `[${from}..${inclusive ? "." : ""}${to}]`
     return new SanityQueryHelper(this, null)
   }
 
+  /**
+   * Getter for the query string.
+   * @returns {string}
+   */
   get query () {
     return `*[${this.queryOptions.join(" && ")}]${generateOrderBy(
       this.order
@@ -98,6 +177,10 @@ class SanityQueryHelper {
     )}`
   }
 
+  /**
+   * Initialize sanityClient and send the finished query.
+   * @returns {Promise}
+   */
   send () {
     const client = sanityClient(this.sanityOptions)
     return client.fetch(this.query)
